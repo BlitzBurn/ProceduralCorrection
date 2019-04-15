7bbcb9aa085549f3f1304bde11338561f0e38941
BlitzBurn <JK98@Live.se>
2019-04-15 16:51:46 +0200

    morestuff

---
+++ Assets/Scripts/PlayerMoveBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBlock : MonoBehaviour
+{
+    public float movementRate;
+    private Vector3 blockPosition;
+    void Start()
+    {
+       blockPosition = transform.position;
+    }
+    //SendMessegeRaycast : CursorSphere
+    void BlockRaycastMoveUp()
+    {
+        blockPosition.x = transform.position.x + movementRate;
+
+        transform.position = blockPosition;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SendMessegeRaycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendMessegeRaycast : MonoBehaviour
+{
+
+    RaycastHit blockHit;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        Vector3 inward = transform.TransformDirection(Vector3.forward) *100;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out blockHit, 100)) {
+                blockHit.transform.SendMessage("BlockRaycastMoveUp", gameObject, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+    }
+}
