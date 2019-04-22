796a0a0d900512ea4e1e9be0ca45fe0659644dc0
BlitzBurn <JK98@Live.se>
2019-04-22 23:11:48 +0200

    minor xommit

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScript : MonoBehaviour
+{
+    private Text gameOver;
+    public GameObject playerCharacter;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayerDeath()
+    {
+
+        Destroy(playerCharacter);
+        return null;
+    }
+}
